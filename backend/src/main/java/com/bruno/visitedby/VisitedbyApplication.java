28203e07af31067bbc154b1acd53fe065f298970
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2024-05-22 13:08:41 -0300

    FEAT: Adicionado backend inicial

---
+++ backend/src/main/java/com/bruno/visitedby/VisitedbyApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.visitedby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitedbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitedbyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bruno/visitedby/VisitedbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.visitedby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitedbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
